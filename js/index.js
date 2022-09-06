772fefd79bbe81c6b9247bd6cbb7be07967d7144
zhukova_oksana <ksy2905@gmail.com>
2022-09-06 08:02:20 +0300

    adding a file index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+const overlay = document.querySelector('.overlay');
+overlay.classList.add('overlay_type_visible');
+
+const modal = document.querySelector('.add-product');
+modal.classList.add('add-product_type_visible');
+
+const productTitle = document.querySelector('.add-product__title');
+const productId = document.querySelector('.edit__text');
+const editButton = document.querySelector('.edit__button');
+const form = document.querySelector('.form-add');
+const discount = document.querySelector('#discount');
+const promoCode = document.getElementsByName('discount');
+const finalPrice = document.querySelector('.form-add__button .final__price');
